c2eb5fabd828e7a180a86d2c38efcca215fcef58
KiWild <guliyevco@gmail.com>
2021-08-31 21:43:00 +0400

    Working from Home

--- index.php
+++ index.php
@@ -1,10 +1,14 @@
 <?php include('includes/header.php'); ?>
 
-    <!-- MAIN CONTENT -->
-    <div class="main-content">
-        <div class="container">
-            Hello
-        </div>
+<!-- MAIN CONTENT -->
+<div class="main-content">
+    <div class="container">
+        <ul>
+            <li>List Item 1</li>
+            <li>List Item 2</li>
+            <li>List Item 3</li>
+        </ul>
     </div>
+</div>
 
 <?php include('includes/footer.php'); ?>
