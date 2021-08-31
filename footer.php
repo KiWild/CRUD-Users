4f56beb2d9dd76d2b986d601f59cf037572e03fd
KiWild <89848341+KiWild@users.noreply.github.com>
2021-08-31 18:51:14 +0400

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,9 @@
+    <!-- FOOTER -->
+    <div class="footer">
+        <div class="container">
+            Footer
+        </div>
+    </div>
+    
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php include('includes/header.php'); ?>
+
+    <!-- MAIN CONTENT -->
+    <div class="main-content">
+        <div class="container">
+            Main
+        </div>
+    </div>
+
+<?php include('includes/footer.php'); ?>
