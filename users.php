e91699d6b386031c5468ec167e0a1ec142fb4e1b
KiWild <89848341+KiWild@users.noreply.github.com>
2021-09-01 07:17:53 +0400

    Add files via upload

---
+++ users.php
@@ -0,0 +1,21 @@
+<?php include('includes/header.php'); ?>
+
+<!-- MAIN CONTENT -->
+<div class="main-content">
+    <div class="container">
+        <h1>Users</h1>
+
+        <table>
+            <th>id</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Username</th>
+            <th>Phone Number</th>
+            <th>Email</th>
+            <th>Actions</th>
+        </table>
+
+    </div>
+</div>
+
+<?php include('includes/footer.php'); ?>
