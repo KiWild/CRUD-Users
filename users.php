3ed941dee124e99102ef00919196039e605d1deb
KiWild <guliyevco@gmail.com>
2021-09-01 07:46:14 +0400

    2021-09-01
    - Userlist table added
    - CSS for Userlist table added
    - Button styling in progress

--- users.php
+++ users.php
@@ -5,14 +5,46 @@
     <div class="container">
         <h1>Users</h1>
 
-        <table>
-            <th>id</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Username</th>
-            <th>Phone Number</th>
-            <th>Email</th>
-            <th>Actions</th>
+        <table class="tbl-usr-lst">
+            <tr>
+                <th>id</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Username</th>
+                <th>Phone Number</th>
+                <th>Email</th>
+                <th>Actions</th>
+            </tr>
+            
+            <tr>
+                <td>1. </td>
+                <td>Ki</td>
+                <td>Wild</td>
+                <td>kiwild</td>
+                <td>0018449700010</td>
+                <td>kiwild@gmail.com</td>
+                <td>
+                    <form action="" method="POST">
+                        <input type="submit" name="submit" value="Update">
+                        <input type="submit" name="submit" value="Delete">
+                    </form>
+                </td>
+            </tr>
+
+            <tr>
+                <td>1. </td>
+                <td>Ki</td>
+                <td>Wild</td>
+                <td>kiwild</td>
+                <td>0018449700010</td>
+                <td>kiwild@gmail.com</td>
+                <td>
+                    <form action="" method="POST">
+                        <input type="submit" name="submit" value="Update" class="btn-upd-usr">
+                        <input type="submit" name="submit" value="Delete" class="btn-del-usr">
+                    </form>
+                </td>
+            </tr>
         </table>
 
     </div>
