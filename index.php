3b90681a74f00cf3755ba510363370ec5e365a3c
Ki Wild <89848341+KiWild@users.noreply.github.com>
2021-09-03 18:42:46 +0400

    Update index.php

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
             <li>List Item 2</li>
             <li>List Item 3</li>
         </ul>
+        <a href="https://www.freecodecamp.org/news/how-to-keep-your-footer-where-it-belongs-59c6aa05c59c/">How to keep your footer where it belongs?</a><br>
     </div>
 </div>
 
